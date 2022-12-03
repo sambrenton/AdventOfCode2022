61f400223716b7297ea7d03ab607e3f58964fa50
Sam Brenton <84792013+sambrenton@users.noreply.github.com>
2022-12-03 16:48:56 +0000

    Update day3.py

--- day3/day3.py
+++ day3/day3.py
@@ -1,8 +1,11 @@
+# Fetch input data
 with open('input.txt') as input_data:
     input_data = [line.strip() for line in input_data.readlines()]
 
-priorities_sum = 0
 
+# Part One
+
+priorities_sum = 0
 for line in input_data:
     first_compartment, second_compartment = line[:int(len(line) / 2)], line[int(len(line) / 2):]
     in_both_compartments = (list(set(first_compartment).intersection(set(second_compartment)))[0])
@@ -12,3 +15,16 @@ for line in input_data:
         priorities_sum += (ord(in_both_compartments) - 96)
 
 print(priorities_sum)
+  
+# Part Two
+
+sum = 0
+for i in range(0, len(input_data), 3):
+    a, b, c = [set(items) for items in input_data[i:i+3]]
+    character = list(a & b & c)[0]
+    if character.isupper():
+        sum += (ord(character) - 38)
+    elif character.islower():
+        sum += (ord(character) - 96)
+
+print(sum)
