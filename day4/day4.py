21b72b14e720c1e8c3ebf3074e5961d9163d25e3
Sam Brenton <84792013+sambrenton@users.noreply.github.com>
2022-12-04 12:48:29 +0000

    Update day4.py

--- day4/day4.py
+++ day4/day4.py
@@ -7,8 +7,8 @@ with open('input.txt') as input_data:
     # Part One
     overlap_count = 0
     for line in input_data:
-        a, b, c, d = re.findall(r'[0-9][0-9]*', line)
-        seta, setb = set(range(int(a), int(b)+1)), set(range(int(c), int(d)+1))
+        a, b, x, y = re.findall(r'[0-9][0-9]*', line)
+        seta, setb = set(range(int(a), int(b)+1)), set(range(int(x), int(y)+1))
         if seta | setb == seta or seta | setb == setb:
             overlap_count += 1
     print(overlap_count)
