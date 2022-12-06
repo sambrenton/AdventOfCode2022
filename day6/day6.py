c1fb0b42c8e7fc3230262af80eb2b2e926823faa
Sam Brenton <84792013+sambrenton@users.noreply.github.com>
2022-12-06 12:06:48 +0000

    Update day6.py

--- day6/day6.py
+++ day6/day6.py
@@ -1,6 +1,3 @@
 with open ('input.txt') as input_data:
     input_data = input_data.readline()
-    for i in range(0, len(input_data)):
-        if  len(set((input_data[i:i+4]))) == 4:
-            print(i+4) # <- Answer
-            exit()
+    print([set(input_data[i:i+4]) for i in range(0, len(input_data))].index([set(input_data[i:i+4]) for i in range(0, len(input_data)) if len(set(input_data[i:i+4])) == 4][0])+4)
