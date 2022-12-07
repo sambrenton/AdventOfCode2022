3b0157139d6e4f7acb56253f61383b655d790fc8
Sam Brenton <84792013+sambrenton@users.noreply.github.com>
2022-12-07 20:24:13 +0000

    Update day7.py

--- day7/day7.py
+++ day7/day7.py
@@ -1,3 +1,4 @@
+# Fetch input data
 with open('input.txt') as input_data:
     input_data = [line.strip('\n') for line in input_data.readlines()]
     current_dir = [r'/']
@@ -5,12 +6,14 @@ with open('input.txt') as input_data:
     for line in input_data[1:]:
         current_dir = current_dir
 
+        # Create current directory path as a list
         if line.startswith('$ cd'):
             if not line.endswith('..'):
                 current_dir.append(current_dir[-1]+line[5:])
             else:
                 current_dir.pop()
-                
+
+        # Save directory names and contents to dictionary       
         if line[0].isdigit():
             for i in range(1, len(current_dir)+1):
                 if current_dir[-i] in dirs:
@@ -18,6 +21,7 @@ with open('input.txt') as input_data:
                 else:
                     dirs[current_dir[-i]]=[line]
 
+# Calculate size of each directory
 totals = []
 for dir, files in dirs.items():
     sizes = [z.split(' ')[0] for z in files]
@@ -25,5 +29,11 @@ for dir, files in dirs.items():
     dirs[dir] = total_size
     totals.append(total_size)
 
+# Part One
 totals = [a for a in totals if a <= 100000]
 print(sum(totals))
+
+# Part Two
+unused_space = 70000000 - (dirs[r'/'])
+required_space = 30000000 - unused_space
+print(min([size for size in dirs.values() if size > required_space]))
